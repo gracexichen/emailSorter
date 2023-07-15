facd02be60776d8f053455878ab8d80193a3e135
tiramisuuuuuuu <134162911+tiramisuuuuuuu@users.noreply.github.com>
2023-07-14 20:26:44 -0700

    Fixed errors!

--- chatgpt.js
+++ chatgpt.js
@@ -1,30 +1,29 @@
 
 function getElement(elementID) {
-  return document.getElementById(elementID)
-}
-
-function renderSummary(json, elementID) { //edits specified element's textContent to the summary made in getSummary()
-  getElement(elementID).textContent = json.summary;
-}
-
-function getSummary(emailObject, elementID) { //comminucates with chatgpt to get summary of inputted email and calls functions to render the result
-    //set up parameter variables for fetch command
-  url = "https://gpt-summarization.p.rapidapi.com/summarize"
-  data = JSON.stringify({"text": emailObject.body, "num_sentences": 1})
-  header = {
-    'x-rapidapi-host': "gpt-summarization.p.rapidapi.com",
-    'x-rapidapi-key': "aa2eba7f8fmsha65f1a2ea651d41p15ae9fjsnaa60265cace5",
-    'content-type': "application/json"
-    }
-
-    //send ("post") parameters to chatgpt api
-  fetch(url, {
-    method: "POST",
-    body: data,
-    headers: header
-    })
-
-    //use the response json (summarized email body put in the summary key) to change the text content of the element
-  .then((response) => response.json())
-  .then((json) => render(json, elementID))
-}
+    return document.getElementById("email"+elementID)
+  }
+  
+  function renderSummary(json, elementID) { //edits specified element's textContent to the summary made in getSummary()
+    getElement(elementID).textContent = json.summary;
+  }
+  
+  function getSummary(emailObject) { //comminucates with chatgpt to get summary of inputted email and calls functions to render the result
+      //set up parameter variables for fetch command
+    url = "https://gpt-summarization.p.rapidapi.com/summarize"
+    data = JSON.stringify({"text": emailObject.body, "num_sentences": 1})
+    header = {
+      'x-rapidapi-host': "gpt-summarization.p.rapidapi.com",
+      'x-rapidapi-key': "aa2eba7f8fmsha65f1a2ea651d41p15ae9fjsnaa60265cace5",
+      'content-type': "application/json"
+      }
+    console.log("summarizing")
+      //send ("post") parameters to chatgpt api
+    fetch(url, {
+      method: "POST",
+      body: data,
+      headers: header
+      })
+      //use the response json (summarized email body put in the summary key) to change the text content of the element
+    .then((response) => response.json())
+    .then((json) => renderSummary(json, emailObject.index))
+  }

239df3052484fc5fd5ed872f25d79eacbb1341c4
tiramisuuuuuuu <134162911+tiramisuuuuuuu@users.noreply.github.com>
2023-07-14 18:33:53 -0700

    more readable >:)

--- chatgpt.js
+++ chatgpt.js
@@ -3,11 +3,11 @@ function getElement(elementID) {
   return document.getElementById(elementID)
 }
 
-function render(output, elementID) {
-  getElement(elementID).textContent = output.summary;
+function renderSummary(json, elementID) { //edits specified element's textContent to the summary made in getSummary()
+  getElement(elementID).textContent = json.summary;
 }
 
-function getSummary(emailObject, elementID) {
+function getSummary(emailObject, elementID) { //comminucates with chatgpt to get summary of inputted email and calls functions to render the result
     //set up parameter variables for fetch command
   url = "https://gpt-summarization.p.rapidapi.com/summarize"
   data = JSON.stringify({"text": emailObject.body, "num_sentences": 1})

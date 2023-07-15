08f1bf5685869c48aab3c51fcbd2812a76eb174b
tiramisuuuuuuu <134162911+tiramisuuuuuuu@users.noreply.github.com>
2023-07-14 18:27:03 -0700

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -1,1 +1,30 @@
 
+function getElement(elementID) {
+  return document.getElementById(elementID)
+}
+
+function render(output, elementID) {
+  getElement(elementID).textContent = output.summary;
+}
+
+function getSummary(emailObject, elementID) {
+    //set up parameter variables for fetch command
+  url = "https://gpt-summarization.p.rapidapi.com/summarize"
+  data = JSON.stringify({"text": emailObject.body, "num_sentences": 1})
+  header = {
+    'x-rapidapi-host': "gpt-summarization.p.rapidapi.com",
+    'x-rapidapi-key': "aa2eba7f8fmsha65f1a2ea651d41p15ae9fjsnaa60265cace5",
+    'content-type': "application/json"
+    }
+
+    //send ("post") data to chatgpt api
+  fetch(url, {
+    method: "POST",
+    body: data,
+    headers: header
+    })
+
+    //use the response json (summarized email body put in the summary key) to change the text content of the element
+  .then((response) => response.json())
+  .then((json) => render(json, elementID))
+}
